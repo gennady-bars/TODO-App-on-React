ef2d8d4d571e4fa529fd70ed3b6e6549bc8a24f7
gennady-bars <54933269+gennady-bars@users.noreply.github.com>
2020-04-05 11:37:26 +0300

    start my new app

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+function TodoItem() {
+  return (
+    <span>Drink Coffee</span>
+  )
+}
+
+function TodoList() {
+  return (
+    <ul>
+      <li><TodoItem></TodoItem></li>
+      <li><TodoItem></TodoItem></li>
+      <li><TodoItem></TodoItem></li>
+    </ul>
+  )
+}
+
+function App() {
+  return (
+    <TodoList/>
+  )
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +34,3 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
